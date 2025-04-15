4432c58a3abf05149e7393eb839f6fd768b26449
korbinian90 <korbinian90@gmail.com>
2025-04-15 15:20:09 +0200

    basic functionality

--- README.md
+++ README.md
@@ -4,3 +4,40 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://korbinian90.github.io/Niivue.jl/dev/)
 [![Build Status](https://github.com/korbinian90/Niivue.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/korbinian90/Niivue.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/korbinian90/Niivue.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/korbinian90/Niivue.jl)
+
+## Quickstart
+
+This should work in almost any julia environment (REPL, vscode, Pluto, Jupyter)
+
+```julia
+using Niivue
+
+nv = niivue()
+display(nv.app) ## currently required to display, run in own cell
+
+volumes = [
+    Dict(
+        :url => "https://niivue.github.io/niivue/images/mni152.nii.gz",
+        :colormap => "gray",
+    ),
+    Dict(
+        :url => "https://niivue.github.io/niivue/images/hippo.nii.gz", 
+        :colormap => "red",
+    )
+]
+
+nv.loadVolumes(volumes)
+nv.setCrosshairWidth(5)
+nv.setCrosshairColor([0,1,1,0.5])
+```
+
+## Electron Display
+
+To use an Electron display, add in the beginning
+
+```julia
+using Electron
+use_electron_display(devtools=false)
+```
+
+In vscode, by default the plot pane is used. To use the Electron display, deactivate the plotting pane via the command palette (ctrl+shift+p) -> Julia: Disable Plot Pane
--- src/Niivue.jl
+++ src/Niivue.jl
@@ -1,5 +1,51 @@
 module Niivue
 
-# Write your package code here.
+using Bonito
+
+export niivue, use_electron_display
+
+function niivue()
+    obs = Observable(["setCrosshairWidth", 5])
+
+    nv_dom = DOM.canvas(id="gl"; width=400, height=400)
+
+    js_eval = js"""
+        import("https://cdn.jsdelivr.net/npm/@niivue/niivue@0.56.0/+esm").then((Niivue) => {
+            const nv = new Niivue.Niivue()
+            nv.attachTo('gl')
+            function nv_call(args) {
+                const [method, arg] = args[0]
+                console.log("niivue call", method, arg)
+                nv[method](arg)
+            }
+            Bonito.onany([$(obs)], nv_call)
+        })
+    """
+
+    app = App() do session
+        obs2 = Observable(["setCrosshairWidth", 5])
+        DOM.div(nv_dom, evaljs(session, js_eval))
+    end
+    return NiivueViewer(app, obs)
+end
+
+struct NiivueViewer
+    app::App
+    obs::Observable
+end
+
+# Overload the call operator to allow calling nv("setCrosshairWidth", 5)
+function (nv::NiivueViewer)(method::String, arg)
+    nv.obs[] = [method, arg]
+end
+
+# Overload dot syntax for e.g. nv.setCrosshairWidth(5)
+function Base.getproperty(nv::NiivueViewer, name::Symbol)
+    if hasfield(typeof(nv), name)
+        return getfield(nv, name)
+    else
+        return value -> nv(string(name), value)
+    end
+end
 
 end
