b470a9efe20b2a402738cdfa1e8246e774529d59
Documenter.jl <documenter@juliadocs.github.io>
2025-04-26 23:51:49 +0000

    build based on cfa56c0

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Niivue","category":"page"},{"location":"#Niivue","page":"Home","title":"Niivue","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Niivue.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Niivue]","category":"page"}]
+[{"location":"#Niivue","page":"Home","title":"Niivue","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Niivue.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
